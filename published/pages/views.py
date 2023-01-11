a0bdc84b6fc05e4064e42ace9777b3b8582daf61
Deniz Karaman <is.denizkaraman@gmail.com>
2023-01-11 20:13:03 +0300

    Published Version Updated

---
+++ published/pages/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from . models import Product, CompanyInfo, SocialMedia
+# Create your views here.
+def index(request):
+    compinfo = CompanyInfo.objects.all()
+    social = SocialMedia.objects.all()
+    context = {
+        "compinfo":compinfo,
+        "social":social,
+    }
+    return render(request, "index.html", context)
+
+def products(request):
+    productinfo = Product.objects.all()
+    compinfo = CompanyInfo.objects.all()
+    social = SocialMedia.objects.all()
+    context = {
+        "compinfo":compinfo,
+        "social":social,
+        "products":productinfo,
+    }
+    return render(request, "urunler.html", context)
+
+def productdetail(request, productslug):
+    productinfo = Product.objects.get(slug=productslug)
+    context = {"product":productinfo,}
+    return render(request, "urundetay.html", context)
